af09ce062f75e6ec8fcf61b096b56fc337d28c49
Sune Carelse <sune.carelse@takealot.com>
2021-01-24 19:55:55 +0200

    created listening port 9000 / setup db config

---
+++ server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import Pusher from 'pusher';
+
+// app config 
+const app = express()
+const port = process.env.POTRT || 9000
+
+// middlewares
+app.use(cors())
+app.use(express.json())
+
+// db config
+const mongoURI = 'mongodb+srv://admin:Vbti2zaP5b4hu3gx@cluster0.wvs32.mongodb.net/<dbname>?retryWrites=true&w=majority'
+
+mongoose.connect(mongooseURI, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+// api routes
+app.get('/', (req, res) => res.status(200).send('Lets build mern stack🚀🚀'))
+
+// listen
+app.listen(port, () => console.log(`listening on localhost:${port}`))
